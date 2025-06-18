c626fe8ed2008433706e963ba98e76a83038a070
Peter Chinoko <52344650+Chinokoo@users.noreply.github.com>
2025-06-18 11:09:21 +0200

    fix typo

--- config/cron.js
+++ config/cron.js
@@ -1,9 +1,9 @@
 import cron from "cron";
 import https from "https";
 
-const job = new cron.CronJob("*/5 * * * * ", function () {
+const job = new cron.CronJob("*/14 * * * * ", () => {
   https
-    .get("https://jobnest-backendd.onrender.com/", (res) => {
+    .get("https://jobnest-backendd.onrender.com", (res) => {
       if (res.statusCode === 200) console.log("GET request is successfully");
       else console.error("GET request failed", res.statusCode);
     })
