76d901d5df4c88922831501502a397d5682cf2e5
Peter Chinoko <52344650+Chinokoo@users.noreply.github.com>
2025-06-18 02:41:44 +0200

    fix typo in the cors origin property

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(express.json({ limit: "5mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://job-nest-r3mbfjqex-peters-projects-2abaeda5.vercel.app/",
+    origin: "https://job-nest-r3mbfjqex-peters-projects-2abaeda5.vercel.app",
     credentials: true,
   })
 );
